0e55bd869f17a20a39e5ec747ec63f941e900e00
Stefan Felder <stefanfelder@bbv.ch>
2024-04-04 18:39:53 +0200

    Sun position is calculated correctly

--- SunTracer_lib/SunTracer.h
+++ SunTracer_lib/SunTracer.h
@@ -6,24 +6,31 @@
 #define GOOGLETEST_CALCULATOR_H
 
 #include <cmath>
+#include <chrono>
+#include <ctime>
 
 class SunTracer {
 public:
-    double getAltitue_Degress(double latitude, double declination, double hourAngle_hour);
-    double getAltitue_Degress(double latitude_Degress, double longitude_Degress, int year, int month, int day, int hour_UTC, int minute, int second);
 
-    double getJulianDate_FROM_UTC(int year, int month, int day, int hour, int minute, int second);
+    double getJulianDate_FROM_UTC(int year, int month, int day, int hour_UTC, int minute, int second);
     double getGMST(double julianDate);
     double getLocalSiderealTime(double gmst, double longitude_Degress);
     double getSunDeclination_Degress(double julianDate);
     double getHourAngle_Degress(double localSiderealTime, double rightAscension);
-
     double getSunRightAscension(double julianDate);
 
-    double calculateSunAzimuth(double latitude_Degress, double declination_Degress, double hourAngle_Degress);
 
 
-    double calculateSunAzimuth(double latitude_Degress, double longitude_Degress, int year, int month, int day, int hour_UTC, int minute, int second);
+    double calculate_sun_azimuth_in_degress(double latitude_Degress, double declination_Degress, double hourAngle_Degress);
+    double calculate_sun_azimuth_in_degress(double latitude_Degress, double longitude_Degress, int year, int month, int day, int hour_UTC, int minute, int second);
+    double calculate_sun_azimuth_in_degress(double latitude, double declination, std::time_t time_UTC);
+
+
+    double calculate_sun_altitue_in_degress(double latitude, double declination, double hourAngle_hour);
+    double calculate_sun_altitue_in_degress(double latitude_Degress, double longitude_Degress, int year, int month, int day, int hour_UTC, int minute, int second);
+    double calculate_sun_altitue_in_degress(double latitude, double declination, std::time_t time_UTC);
+
+
 
 
 private:
